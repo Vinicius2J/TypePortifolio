609923238f62f7e25b9b91ff7f3dee8e332af92d
Jose Vinicius <jose.vinicius.batista292@gmail.com>
2023-05-05 00:04:08 -0300

    Novos exercicios adicionado na aba algoritmo C

--- scripts/mostrarExeplicações.js
+++ scripts/mostrarExeplicações.js
@@ -33,3 +33,42 @@ function ocultarExplicacaoCinema() {
 
     document.getElementById('entrarNoCinema').style.display = 'none';
 }
+
+//------------------------------------------------------------------------------
+
+let btnExplicaçãoAbertaPd = document.getElementById('mostrarExplicaçãoPd');
+let btnExplicaçãoFechadaPd = document.getElementById('fecharExplicaçãoPd');
+
+function exibirExplicacaoPd() {
+    btnExplicaçãoAbertaPd.style.display = 'none';
+    btnExplicaçãoFechadaPd.style.display = 'flex';
+
+    document.getElementById('pegandoDados01').style.display = 'flex';
+}
+
+function ocultarExplicacaoPd() {
+    btnExplicaçãoAbertaPd.style.display = 'flex';
+    btnExplicaçãoFechadaPd.style.display = 'none';
+
+    document.getElementById('pegandoDados01').style.display = 'none';
+}
+
+//____________parte2__________________________
+
+let btnExplicaçãoAbertaPd02 = document.getElementById('mostrarExplicaçãoPd02');
+let btnExplicaçãoFechadaPd02 = document.getElementById('fecharExplicaçãoPd02');
+
+function exibirExplicacaoPd02() {
+    btnExplicaçãoAbertaPd02.style.display = 'none';
+    btnExplicaçãoFechadaPd02.style.display = 'flex';
+
+    document.getElementById('pegandoDados02').style.display = 'flex';
+}
+
+function ocultarExplicacaoPd02() {
+    btnExplicaçãoAbertaPd02.style.display = 'flex';
+    btnExplicaçãoFechadaPd02.style.display = 'none';
+
+    document.getElementById('pegandoDados02').style.display = 'none';
+}
+
